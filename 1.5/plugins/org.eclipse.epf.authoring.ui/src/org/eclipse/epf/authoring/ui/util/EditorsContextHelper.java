2debaab7f681020a7f12addae01a45a84b3ea789
storaskar <storaskar>
2008-03-28 17:28:34 +0000

    [huxd] set context sensitive help for contentpackage and method configuration

--- 1.5/plugins/org.eclipse.epf.authoring.ui/src/org/eclipse/epf/authoring/ui/util/EditorsContextHelper.java
+++ 1.5/plugins/org.eclipse.epf.authoring.ui/src/org/eclipse/epf/authoring/ui/util/EditorsContextHelper.java
@@ -12,11 +12,13 @@ package org.eclipse.epf.authoring.ui.util;
 
 import org.eclipse.epf.authoring.ui.AuthoringUIHelpContexts;
 import org.eclipse.epf.library.util.LibraryUtil;
+import org.eclipse.epf.uma.ContentPackage;
 import org.eclipse.epf.uma.CustomCategory;
 import org.eclipse.epf.uma.Discipline;
 import org.eclipse.epf.uma.DisciplineGrouping;
 import org.eclipse.epf.uma.Domain;
 import org.eclipse.epf.uma.Guidance;
+import org.eclipse.epf.uma.MethodConfiguration;
 import org.eclipse.epf.uma.MethodPlugin;
 import org.eclipse.epf.uma.Role;
 import org.eclipse.epf.uma.RoleSet;
@@ -131,6 +133,16 @@ public class EditorsContextHelper {
 		if ((obj instanceof MethodPlugin)) {
 			PlatformUI.getWorkbench().getHelpSystem().setHelp(uiComposite,
 					AuthoringUIHelpContexts.PLUGIN_EDITOR_DESCRIPTION_ALL_CONTEXT);
+			return;
+		}
+		if (obj instanceof ContentPackage) {
+			PlatformUI.getWorkbench().getHelpSystem().setHelp(uiComposite,
+					AuthoringUIHelpContexts.CONTENT_PACKAGE_EDITOR_ALL_CONTEXT);
+			return;
+		}
+		if (obj instanceof MethodConfiguration) {
+			PlatformUI.getWorkbench().getHelpSystem().setHelp(uiComposite,
+					AuthoringUIHelpContexts.CONFIGURATION_EDITOR_ALL_CONTEXT);
 			return;
 		}
 		
