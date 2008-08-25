0c3b5194e497332d74f9f0eaae32de1e6ee5fb1e
jtham <jtham>
2008-08-25 01:02:02 +0000

    add NL2 plugins

---
+++ 1.5/nls/nl2/plugins/org.eclipse.epf.library.nl2/layout/scripts/ContentPageResource_pl.js
@@ -0,0 +1,37 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2000, 2007 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+/* resource files for content page javascript 
+ * this file should be localized
+*/
+ContentPageResource = function() {
+
+
+this.expandAllText = "Rozwiń wszystkie sekcje";
+this.collapseAllText = "Zwiń wszystkie sekcje";
+this.backToTopText = "Początek strony";
+
+this.expandAllSubSectionsText = "Rozwiń wszystkie kroki";
+this.collapseAllSubSectionsText = "Zwiń wszystkie kroki";
+
+this.toolbar_bookmark = "Utwórz zakładkę dla tej strony";
+this.toolbar_back = "Wstecz";
+this.toolbar_forward = "Dalej";
+this.toolbar_expand_all = "Rozwiń wszystkie";
+this.toolbar_collapse_all = "Zwiń wszystkie";
+this.toolbar_print = "Drukuj";
+
+this.expandText = "Rozwiń"
+this.collapseText = "Zwiń"
+this.checkedAltText = "Tak"	
+
+};
+
---
+++ 1.5/nls/nl2/plugins/org.eclipse.epf.library.nl2/layout/scripts/ContentPageResource_ru.js
@@ -0,0 +1,37 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2000, 2007 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+/* resource files for content page javascript 
+ * this file should be localized
+*/
+ContentPageResource = function() {
+
+
+this.expandAllText = "Развернуть все разделы";
+this.collapseAllText = "Свернуть все разделы";
+this.backToTopText = "В начало";
+
+this.expandAllSubSectionsText = "Развернуть все шаги";
+this.collapseAllSubSectionsText = "Свернуть все шаги";
+
+this.toolbar_bookmark = "Добавить закладку на эту страницу";
+this.toolbar_back = "Назад";
+this.toolbar_forward = "Вперед";
+this.toolbar_expand_all = "Развернуть все";
+this.toolbar_collapse_all = "Свернуть все";
+this.toolbar_print = "Печать";
+
+this.expandText = "Развернуть "
+this.collapseText = "Свернуть "
+this.checkedAltText = "Да "	
+
+};
+
