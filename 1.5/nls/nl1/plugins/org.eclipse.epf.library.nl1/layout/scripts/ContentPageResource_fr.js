4898bbb4b13e88934cf5205692967af758fd4f06
jtham <jtham>
2008-08-25 00:52:16 +0000

    add NL1 plugins

---
+++ 1.5/nls/nl1/plugins/org.eclipse.epf.library.nl1/layout/scripts/ContentPageResource_fr.js
@@ -0,0 +1,37 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2000, 2007 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+/* resource files for content page javascript 
+ * this file should be localized
+*/
+ContentPageResource = function() {
+
+
+this.expandAllText = "Développer toutes les sections";
+this.collapseAllText = "Condenser toutes les sections";
+this.backToTopText = "Haut de page";
+
+this.expandAllSubSectionsText = "Développer toutes les étapes";
+this.collapseAllSubSectionsText = "Condenser toutes les étapes";
+
+this.toolbar_bookmark = "Marquer cette page";
+this.toolbar_back = "Retour";
+this.toolbar_forward = "Suivant";
+this.toolbar_expand_all = "Développer tout";
+this.toolbar_collapse_all = "Réduire tout";
+this.toolbar_print = "Imprimer";
+
+this.expandText = "Développer"
+this.collapseText = "Réduire"
+this.checkedAltText = "Oui"	
+
+};
+
---
+++ 1.5/nls/nl1/plugins/org.eclipse.epf.library.nl1/layout/scripts/ContentPageResource_pt_BR.js
@@ -0,0 +1,37 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2000, 2007 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+/* resource files for content page javascript 
+ * this file should be localized
+*/
+ContentPageResource = function() {
+
+
+this.expandAllText = "Expandir Todas as Seções";
+this.collapseAllText = "Reduzir Todas as Seções";
+this.backToTopText = "Voltar ao Início da Página";
+
+this.expandAllSubSectionsText = "Expandir Todas as Etapas";
+this.collapseAllSubSectionsText = "Reduzir Todas as Etapas";
+
+this.toolbar_bookmark = "Indicar esta página";
+this.toolbar_back = "Voltar";
+this.toolbar_forward = "Avançar";
+this.toolbar_expand_all = "Expandir Tudo";
+this.toolbar_collapse_all = "Reduzir Tudo";
+this.toolbar_print = "Imprimir";
+
+this.expandText = "Expandir"
+this.collapseText = "Reduzir"
+this.checkedAltText = "Sim"	
+
+};
+
