fb004bc215644e1d7a54b01a69e46c9be7a440b2
storaskar <storaskar>
2008-09-18 21:27:32 +0000

    Adding more checks for valid hex string

--- 1.5/plugins/org.eclipse.epf.common/src/org/eclipse/epf/common/utils/StrUtil.java
+++ 1.5/plugins/org.eclipse.epf.common/src/org/eclipse/epf/common/utils/StrUtil.java
@@ -365,17 +365,27 @@ public class StrUtil {
 			case '%':
 				if (i + 4 < length) {
 					String hexStr = html.substring(i + 1, i + 5);
-					try {
-						int codePoint = Integer.parseInt(hexStr, 16);
-						char[] c = UCharacter.toChars(codePoint);
-						result.append(c);
-						i += 4;
-						break;
-					} catch (NumberFormatException e) {
-						// wasn't a valid hex string..
-						// fall through to the result.append(ch)
-					} catch (Exception e) {
-						CommonPlugin.getDefault().getLogger().logError(e);
+					boolean validHextStr = true;
+					for (int j = 0; j < hexStr.length(); j++) {
+						char c = hexStr.charAt(j);
+						if (!((c >= '0' && c <= '9') || (c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F'))) {
+							validHextStr = false;
+							break;
+						}
+					}
+					if (validHextStr) {
+						try {
+							int codePoint = Integer.parseInt(hexStr, 16);
+							char[] c = UCharacter.toChars(codePoint);
+							result.append(c);
+							i += 4;
+							break;
+						} catch (NumberFormatException e) {
+							// wasn't a valid hex string..
+							// fall through to the result.append(ch)
+						} catch (Exception e) {
+							CommonPlugin.getDefault().getLogger().logError(e);
+						}
 					}
 				}
 				result.append(ch);
