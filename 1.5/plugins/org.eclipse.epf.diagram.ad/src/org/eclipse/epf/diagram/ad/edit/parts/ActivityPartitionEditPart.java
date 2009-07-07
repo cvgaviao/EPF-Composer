b7f17ab472f30605f1dd514232c176e7f71cf1bb
storaskar <storaskar>
2009-07-07 22:31:59 +0000

    01483134: Activity Partition Fill Color doesn't do anything.

--- 1.5/plugins/org.eclipse.epf.diagram.ad/src/org/eclipse/epf/diagram/ad/edit/parts/ActivityPartitionEditPart.java
+++ 1.5/plugins/org.eclipse.epf.diagram.ad/src/org/eclipse/epf/diagram/ad/edit/parts/ActivityPartitionEditPart.java
@@ -345,7 +345,11 @@ public class ActivityPartitionEditPart extends ShapeNodeEditPart {
 
 		@Override
 		public void paintFigure(Graphics graphics) {
-			if (isOpaque())
+// this was overridden to resolve label overlap issue for all the nodes but this breaks activity partition fill color and 
+// introduced "01483134: Activity Partition Fill Color doesn't do anything."
+// most likely we don't have any issue with label overlap for activity partition
+// hence commenting it to fix fill color--- shilpat
+//			if (isOpaque())
 				super.paintFigure(graphics);
 			return;
 		}
