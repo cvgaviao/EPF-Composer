47ef27f649c6ea52df5b6ee759bb390a43c900e2
klow <klow>
2010-02-16 16:53:18 +0000

    wlu: temporarily change 1.0.6 to 1.0.5 for convenience of dev. Need to change back later.

--- 1.5/plugins/org.eclipse.epf.uma/src/org/eclipse/epf/uma/impl/UmaFactoryImpl.java
+++ 1.5/plugins/org.eclipse.epf.uma/src/org/eclipse/epf/uma/impl/UmaFactoryImpl.java
@@ -129,7 +129,7 @@ public class UmaFactoryImpl extends EFactoryImpl implements UmaFactory {
 	public static UmaFactory init() {
 		try {
 			UmaFactory theUmaFactory = (UmaFactory) EPackage.Registry.INSTANCE
-					.getEFactory("http://www.eclipse.org/epf/uma/1.0.6/uma.ecore"); //$NON-NLS-1$ 
+					.getEFactory("http://www.eclipse.org/epf/uma/1.0.5/uma.ecore"); //$NON-NLS-1$ 
 			if (theUmaFactory != null) {
 				return theUmaFactory;
 			}
