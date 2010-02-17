8b590359dca12a719a41b5b8f7227b972ed92e9d
klow <klow>
2010-02-17 23:44:18 +0000

    wlu: initial files

---
+++ 1.5/tests/org.eclipse.epf.toolbox/src/org/eclipse/epf/toolbox/actions/ConvertTosynFree.java
@@ -0,0 +1,34 @@
+package org.eclipse.epf.toolbox.actions;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+
+public class ConvertTosynFree implements IWorkbenchWindowActionDelegate {
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void init(IWorkbenchWindow window) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void run(IAction action) {
+		// TODO Auto-generated method stub
+		System.out.println("LD> Hi, I am running!");
+	}
+
+	@Override
+	public void selectionChanged(IAction action, ISelection selection) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
