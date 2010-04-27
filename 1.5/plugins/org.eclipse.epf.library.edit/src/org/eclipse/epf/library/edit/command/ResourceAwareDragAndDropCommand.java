91eadccea98ce236b16de038e83259e7dc8ba299
klow <klow>
2010-04-27 05:29:11 +0000

    wlu: fixed a bug that the modified config due to drag and drop would not get persisted

--- 1.5/plugins/org.eclipse.epf.library.edit/src/org/eclipse/epf/library/edit/command/ResourceAwareDragAndDropCommand.java
+++ 1.5/plugins/org.eclipse.epf.library.edit/src/org/eclipse/epf/library/edit/command/ResourceAwareDragAndDropCommand.java
@@ -65,9 +65,19 @@ implements IResourceAwareCommand
 	 */
 	@SuppressWarnings("unchecked") 
 	public Collection<Resource> getModifiedResources() {
+		modifiedResources = null;	//wlu: Disable cache - cache without update for change would not work
+		
 		if(modifiedResources == null) {
 			if(dropCommand != null) {
 				modifiedResources = TngUtil.getModifiedResources(dropCommand);
+			}
+			if(dragCommand != null) {
+				Set<Resource> resources = TngUtil.getModifiedResources(dragCommand);
+				if (modifiedResources != null) {
+					modifiedResources.addAll(resources);
+				} else {
+					modifiedResources = resources;
+				}
 			}
 			if(modifiedResources == null || modifiedResources.isEmpty()) {
 				modifiedResources = Collections.EMPTY_SET;
