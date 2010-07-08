4c4bd29045018a1107f9bf82392b615ed88116f0
klow <klow>
2010-07-08 19:14:47 +0000

    wlu: modified

--- 1.5/tests/org.eclipse.epf.toolbox/src/org/eclipse/epf/toolbox/actions/ConvertToConfigFree.java
+++ 1.5/tests/org.eclipse.epf.toolbox/src/org/eclipse/epf/toolbox/actions/ConvertToConfigFree.java
@@ -1,23 +1,34 @@
 package org.eclipse.epf.toolbox.actions;
 
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.epf.authoring.ui.editors.EditorChooser;
 import org.eclipse.epf.library.LibraryService;
+import org.eclipse.epf.library.LibraryServiceUtil;
 import org.eclipse.epf.library.configuration.ConfigurationHelper;
-import org.eclipse.epf.library.util.LibraryUtil;
+import org.eclipse.epf.persistence.MultiFileXMIResourceImpl;
+import org.eclipse.epf.persistence.refresh.RefreshJob;
+import org.eclipse.epf.services.ILibraryPersister;
 import org.eclipse.epf.toolbox.ToolboxPlugin;
 import org.eclipse.epf.toolbox.libutil.LibUtil;
 import org.eclipse.epf.uma.MethodLibrary;
 import org.eclipse.epf.uma.Process;
 import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.ProgressMonitorDialog;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+import org.eclipse.ui.actions.WorkspaceModifyOperation;
 
 public class ConvertToConfigFree implements IWorkbenchWindowActionDelegate {
 
 	private static boolean debug = true;			//ToolboxPlugin.getDefault().isDebugging();
+	private Set<Resource> resouresToSave = new HashSet<Resource>();
 	
 	public void dispose() {
 		// TODO Auto-generated method stub
@@ -30,6 +41,40 @@ public class ConvertToConfigFree implements IWorkbenchWindowActionDelegate {
 	}
 
 	public void run(IAction action) {
+		
+		boolean oldValue = RefreshJob.getInstance().isEnabled();
+		RefreshJob.getInstance().setEnabled(false);
+		
+		final IAction a = action;
+		WorkspaceModifyOperation operation = new WorkspaceModifyOperation() {
+			public void execute(IProgressMonitor monitor) {
+				monitor.beginTask("Convert processes to config-free ... ",
+						10);
+				monitor.worked(3);
+				monitor.setTaskName("Convert processes to config-free ...");
+				try {
+					run_(a);
+				} finally {
+					monitor.done();
+				}
+			}
+		};
+
+		try {
+			// Run the operation and display the progress.
+			ProgressMonitorDialog pmDialog = new ProgressMonitorDialog(Display
+					.getCurrent().getActiveShell());
+			pmDialog.run(true, false, operation);
+		} catch (Exception e) {
+			ToolboxPlugin.getDefault().getLogger().logError(e);
+		} finally {
+			if (oldValue) {
+				RefreshJob.getInstance().setEnabled(true);
+			}	
+		}
+	}
+	
+	private void run_(IAction action) {
 		if (debug) {
 			LibUtil.trace("LD> Begin: ConvertToConfigFree.run()");
 		}
@@ -46,22 +91,25 @@ public class ConvertToConfigFree implements IWorkbenchWindowActionDelegate {
 		int n = 0;
 		for (Process proc : processes) {
 			if (ConfigurationHelper.getDelegate().canBeConfigFree(proc)) {
-				proc.setDefaultContext(null);
-				proc.getValidContext().clear();
-				n++;
+				if (proc.getDefaultContext() != null
+						|| !proc.getValidContext().isEmpty()) {
+					proc.setDefaultContext(null);
+					proc.getValidContext().clear();
+					n++;
+					Resource res = proc.eResource();
+					if (res != null) {
+						resouresToSave.add(res);
+					}
+				}
 			}
 		}
 		if (debug) {
-			LibUtil.trace("LD> number of processes converted: " + n);
-			LibUtil.trace("LD> number of processes not converted: " + (processes.size() - n));
+			LibUtil.trace("LD> number of processes touched: " + n);
+			LibUtil.trace("LD> number of processes not touched: " + (processes.size() - n));
 		}
 		
 		if ( n > 0) {
-			try {
-				LibraryUtil.saveAll(lib);
-			} catch (Exception e) {
-				ToolboxPlugin.getDefault().getLogger().logError(e);
-			}
+			save();
 		}
 		
 		if (debug) {
@@ -72,6 +120,28 @@ public class ConvertToConfigFree implements IWorkbenchWindowActionDelegate {
 	public void selectionChanged(IAction action, ISelection selection) {
 		// TODO Auto-generated method stub
 
+	}
+	
+	private void save() {
+		ILibraryPersister.FailSafeMethodLibraryPersister persister = LibraryServiceUtil
+				.getCurrentPersister().getFailSafePersister();
+
+		try {
+			for (Iterator<Resource> it = resouresToSave.iterator(); it
+					.hasNext();) {
+				MultiFileXMIResourceImpl res = (MultiFileXMIResourceImpl) it
+						.next();
+				persister.save(res);
+			}
+			persister.commit();
+
+		} catch (Exception e) {
+			persister.rollback();
+			e.printStackTrace();
+
+		} finally {
+
+		}
 	}
 
 }
