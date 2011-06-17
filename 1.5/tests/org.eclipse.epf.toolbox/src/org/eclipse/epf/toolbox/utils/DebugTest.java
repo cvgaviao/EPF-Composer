6b9bafce07f2a32f33e766921acf1149ca8ae89b
klow <klow>
2011-06-17 21:42:33 +0000

    wlu: modified

--- 1.5/tests/org.eclipse.epf.toolbox/src/org/eclipse/epf/toolbox/utils/DebugTest.java
+++ 1.5/tests/org.eclipse.epf.toolbox/src/org/eclipse/epf/toolbox/utils/DebugTest.java
@@ -1,9 +1,13 @@
 package org.eclipse.epf.toolbox.utils;
 
+import org.eclipse.jface.action.IAction;
+
 public class DebugTest {
 
 	public static boolean debugTestMode = true;
-	public static void run() {
+	public static void run(IAction action) {
+		
+		System.out.println("LD> DebugTest.run");
 		
 	}
 	
